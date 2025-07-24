9059a2d9148d53937e9b23da251a1f61c340a08d
= <artjomsfedosovs2@gmail.com>
2025-07-24 10:05:28 +0300

    test(scanner): Add more unit tests for checking IPs and ports validation

--- tests/test_scanner.cpp
+++ tests/test_scanner.cpp
@@ -3,41 +3,72 @@
 #include "../include/scanner.hpp"
 
 TEST(ScannerTest, ValidIPv4Address) {
-        EXPECT_NO_THROW(
-                Scanner scanner ("127.0.0.1", std::vector<char const *> {});
-        );
+        std::array<ipaddr_t, 5> arr {"127.0.0.1", "192.168.8.10", "132.89.0.12",
+                "90.67.12.123", "97.30.231.251"};
+
+        for (auto const &el : arr) {
+                EXPECT_NO_THROW(
+                        Scanner scanner (el, std::vector<char const *> {})
+                );
+        }
 }
 
 TEST(ScannerTest, ValidIPv6Address) {
-        EXPECT_NO_THROW(
-                Scanner scanner ("::1", std::vector<char const *> {});
-        );
+        std::array<ipaddr_t, 5> arr {"::1",
+                "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+                "fd12:3456:789a:0001::abcd",
+                "2a02:6b8:0:1::1234",
+                "fe80::0202:b3ff:fe1e:8329"};
+
+        for (auto const &el : arr) {
+                EXPECT_NO_THROW(
+                        Scanner scanner (el, std::vector<char const *> {})
+                );
+        }
 }
 
 TEST(ScannerTest, InvalidIPAddress) {
-        EXPECT_THROW(
-                Scanner scanner("invalid_ip", std::vector<char const *> {}),
-                     std::invalid_argument
-        );
+        std::array<ipaddr_t, 5> arr {"invalid_ip", "", "112345.1235.12345.1235",
+                "256.256.256.256", "255.255.255.-1"};
+
+        for (auto const &el : arr) {
+                EXPECT_THROW(
+                        Scanner scanner (el, std::vector<char const *> {}),
+                                std::invalid_argument
+                );
+        }
 }
 
 TEST(ScannerTest, ValidPort) {
+        std::vector<char const *> vec {"80", "53", "10", "0", "65535", "123"};
+
         EXPECT_NO_THROW(
-                Scanner scanner ("127.0.0.1",
-                        std::vector<char const *> {"80", "53", "10"});
+                Scanner scanner ("127.0.0.1", vec);
         );
 }
 
 TEST(ScannerTest, InvalidPort) {
         EXPECT_THROW(
                 Scanner scanner("127.0.0.1",
-                        std::vector<char const *> {"80", "70000"}),
+                        std::vector<char const *> {"70000"}),
                 std::invalid_argument
         );
 
         EXPECT_THROW(
-                Scanner scanner("abc",
-                        std::vector<char const *> {"80", "70000"}),
+                Scanner scanner("127.0.0.1",
+                                std::vector<char const *> {"-1"}),
+                std::invalid_argument
+        );
+
+        EXPECT_THROW(
+                Scanner scanner("127.0.0.1",
+                                std::vector<char const *> {"65536"}),
+                std::invalid_argument
+        );
+
+        EXPECT_THROW(
+                Scanner scanner("127.0.0.1",
+                                std::vector<char const *> {"-234"}),
                 std::invalid_argument
         );
 }
