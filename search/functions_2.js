1c3620cad640cae0885bcd0e08b9f4ca2df88b9d
artyom-fedosov <artyom-fedosov@users.noreply.github.com>
2025-08-08 10:15:01 +0000

    deploy: 994b649bd447742b5ec9287ffee4ac673d75cae1

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['parseport_0',['parsePort',['../classScanner.html#a8c0c87368ad65fedb1d3e83da5853666',1,'Scanner']]],
-  ['print_1',['print',['../classPrinter.html#ae5797dfbc6e50752b40c687a5a0233c8',1,'Printer']]]
+  ['print_1',['print',['../classPrinter.html#aaeb1722ebd8cb9fc7b47e828dba55707',1,'Printer']]]
 ];
