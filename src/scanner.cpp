1bfcfe9b012571d197497bddce749573bced8bfd
artyom-fedosov <artjomsfedosovs2@gmail.com>
2025-08-23 10:11:16 +0300

    refactor: encapsulate data and functions within Scanner class

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -1,6 +1,8 @@
 #include "../include/scanner.hpp"
 
 #include <charconv>
+#include <stdexcept>
+#include <thread>
 
 #include <cstring>
 
@@ -10,36 +12,71 @@
 #include <netdb.h>
 #include <fcntl.h>
 
-std::mutex mtx;
+Scanner::Scanner(const ipaddr_t &ip, const std::vector<const char *> &ports)
+                : ip_ {ip}, ports_ {}, mtx_ {} {
+        if (!isIPv4(ip_) and !isIPv6(ip_))
+                throw std::invalid_argument {"IP address is not valid!"};
 
-[[nodiscard]] bool isIPv4(const ipaddr_t &ip) {
+        for (const auto prt : ports) {
+                std::optional<port_t> port {parsePort(prt)};
+                if (port)
+                        ports_.push_back(port.value());
+                else
+                        throw std::invalid_argument {std::string {prt} +
+                                " is not valid port!"};
+        }
+}
+
+[[nodiscard]] std::vector<std::pair<port_t, bool>>
+Scanner::scan() noexcept {
+        std::vector<std::pair<port_t, bool>> result {};
+        std::vector<std::thread> threads {};
+        for (const auto port : ports_) {
+                threads.push_back(std::thread {[this, port, &result]() {
+                        const bool isAccessible {isPortAccessible(port)};
+                        mtx_.lock();
+                        result.push_back(std::pair {port, isAccessible});
+                        mtx_.unlock();
+                }});
+        }
+
+        for (auto &thread : threads)
+                thread.join();
+
+        return result;
+}
+
+[[nodiscard]] bool Scanner::isIPv4(const ipaddr_t &ip) const noexcept {
         in_addr address;
         return inet_pton(AF_INET, ip.c_str(), &address) == 1;
 }
 
-[[nodiscard]] bool isIPv6(const ipaddr_t &ip) {
+[[nodiscard]] bool Scanner::isIPv6(const ipaddr_t &ip) const noexcept {
         in6_addr address6;
         return inet_pton(AF_INET6, ip.c_str(), &address6) == 1;
 }
 
-[[nodiscard]] std::optional<port_t> parsePort(const char *str) {
+[[nodiscard]] std::optional<port_t>
+Scanner::parsePort(const char *str) const noexcept {
         int prt;
         const char *strEnd = str + std::strlen(str);
         const std::from_chars_result res = std::from_chars(str, strEnd, prt);
 
-        if (res.ec != std::errc() or prt < MIN_PORT or prt > MAX_PORT or res.ptr != strEnd)
+        if (res.ec != std::errc() or prt < MIN_PORT or prt > MAX_PORT or
+            res.ptr != strEnd)
                 return {};
 
         return {static_cast<port_t>(prt)};
 }
 
-[[nodiscard]] bool isPortAccessible(const ipaddr_t &ip, const port_t port) {
+[[nodiscard]] bool Scanner::isPortAccessible(const port_t port) const noexcept {
         addrinfo hints {}, *res = nullptr;
         hints.ai_family = AF_UNSPEC;
         hints.ai_socktype = SOCK_STREAM;
 
         std::string portStr = std::to_string(port);
-        if (getaddrinfo(ip.c_str(), portStr.c_str(), &hints, &res) != 0 || !res)
+        if (getaddrinfo(ip_.c_str(), portStr.c_str(), &hints, &res) != 0 ||
+                        !res)
                 return false;
 
         const int sockfd = socket(res->ai_family, SOCK_STREAM, 0);
@@ -48,7 +85,8 @@ std::mutex mtx;
                 return false;
         }
 
-        constexpr timeval timeout = {TIMEOUT_MS / 1000, (TIMEOUT_MS % 1000) * 1000l};
+        constexpr timeval timeout {TIMEOUT_MS / 1000,
+                (TIMEOUT_MS % 1000) * 1000l};
         setsockopt(sockfd, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout));
 
         const int result = connect(sockfd, res->ai_addr, res->ai_addrlen);
@@ -56,8 +94,4 @@ std::mutex mtx;
         close(sockfd);
 
         return result == 0;
-}
-
-[[nodiscard]] bool isTerminal() {
-        return isatty(fileno(stdout));
 }
