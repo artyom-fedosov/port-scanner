4980267e6b5cc67d95c03984d7f0bdeac9babb00
artyom-fedosov <artjomsfedosovs2@gmail.com>
2025-08-24 22:51:32 +0300

    feat(main): update exception handling

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,25 @@ int main(int argc, char *argv[]) {
                 return 1;
         }
 
-        ipaddr_t ip {argv[1]};
-        std::vector<char const *> ports (&argv[2], &argv[argc]);
         try {
-                Scanner scanner {ip, ports};
+                ipaddr_t const ip {argv[1]};
+                std::vector<char const *> const ports (&argv[2], &argv[argc]);
+                Scanner const scanner {ip, ports};
                 Printer::print(scanner.scan());
-        } catch (std::exception &e) {
-                std::cerr << e.what() << '\n';
+        } catch (std::length_error &e) {
+                std::cerr << "Length error: " << e.what() << '\n';
                 return 2;
+        } catch (std::bad_alloc &e) {
+                std::cerr << "Memory allocation failed: " << e.what() << '\n';
+                return 3;
+        } catch (std::invalid_argument &e) {
+                std::cerr << "Invalid argument: " << e.what() << '\n';
+                return 4;
+        } catch (std::system_error &e) {
+                std::cerr << "System error: " << e.what() << '\n';
+                return 5;
+        } catch (...) {
+                std::cerr << "Unexpected error occurred.\n";
+                return -1;
         }
 }
