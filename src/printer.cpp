87b9d0ac7fae97bfc69d491e3bf620391ea36612
artyom-fedosov <artjomsfedosovs2@gmail.com>
2025-09-12 20:15:26 +0300

    style(printer): remove excess empty line

--- src/printer.cpp
+++ src/printer.cpp
@@ -12,7 +12,6 @@ void Printer::print(const std::vector<std::pair<port_t, bool>> &ports) {
         bool const isTerm {isTerminal()};
 
         for (auto const &port : ports) {
-
                 if (isTerm) [[likely]] {
                         std::println("{}{}\tis{}accessible{}",
                                 port.second ? GREEN : RED,
